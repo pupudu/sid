177aa039de8d8fef142858a20e19e6d73a10f6f8
Shan1024 <gambit1024@gmail.com>
2015-06-18 15:06:57 +0530

    Linkedin passport auth added

--- test apps/node-authentication-all/config/passport.js
+++ test apps/node-authentication-all/config/passport.js
@@ -382,47 +382,80 @@ module.exports = function(passport) {
       consumerKey   : configAuth.linkedinAuth.consumerKey,
       consumerSecret: configAuth.linkedinAuth.consumerSecret,
       callbackURL   : configAuth.linkedinAuth.callbackURL,
+      passReqToCallback : true, // allows us to pass in the req from our route (lets us check if a user is logged in or not)
       profileFields: ['id', 'first-name', 'last-name', 'email-address']
         },
-        function(token, refreshToken, profile, done) {
 
-        // asynchronous
-        process.nextTick(function() {
+        function(req, token, refreshToken, profile, done) {
 
-            // find the user in the database based on their linked id
-            User.findOne({ 'linkedin.id' : profile.id }, function(err, user) {
+            console.log(profile);
 
-                // if there is an error, stop everything and return that
-                // ie an error connecting to the database
-                if (err)
-                    return done(err);
+            // asynchronous
+            process.nextTick(function() {
 
-                // if the user is found, then log them in
-                if (user) {
-                    return done(null, user); // user found, return that user
-                } else {
-                    // if there is no user found with that linkedin id, create them
-                    var newUser            = new User();
+                // check if the user is already logged in
+                if (!req.user) {
 
-                    // set all of the facebook information in our user model
-                    newUser.linkedin.id    = profile.id; // set the users linkedin id
-                    newUser.linkedin.token = token; // we will save the token that linkedin provides to the user
-                    newUser.linkedin.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-                    newUser.linkedin.email = (profile.emails[0].value || '').toLowerCase(); // linkedin can return multiple emails so we'll take the first
-                    //console.log(profile);
-                    // save our user to the database
-                    newUser.save(function(err) {
+                    User.findOne({ 'linkedin.id' : profile.id }, function(err, user) {
                         if (err)
-                            throw err;
+                            return done(err);
 
-                        // if successful, return the new user
-                        return done(null, newUser);
+                        if (user) {
+
+                            // if there is a user id already but no token (user was linked at one point and then removed)
+                            if (!user.linkedin.token) {
+                                user.linkedin.token = token;
+                                user.linkedin.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                                user.linkedin.email = (profile.emails[0].value || '').toLowerCase();
+
+                                console.log("USER: "+user);
+
+                                user.save(function(err) {
+                                    if (err)
+                                        return done(err);
+
+                                    return done(null, user);
+                                });
+                            }
+
+                            return done(null, user); // user found, return that user
+                        } else {
+                            // if there is no user, create them
+                            var newUser            = new User();
+
+                            newUser.linkedin.id    = profile.id;
+                            newUser.linkedin.token = token;
+                            newUser.linkedin.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                            newUser.linkedin.email = (profile.emails[0].value || '').toLowerCase();
+
+                            newUser.save(function(err) {
+                                if (err)
+                                    return done(err);
+
+                                return done(null, newUser);
+                            });
+                        }
                     });
+
+                } else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user            = req.user; // pull the user out of the session
+
+                    user.linkedin.id    = profile.id;
+                    user.linkedin.token = token;
+                    user.linkedin.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                    user.linkedin.email = (profile.emails[0].value || '').toLowerCase();
+
+                    user.save(function(err) {
+                        if (err)
+                            return done(err);
+
+                        return done(null, user);
+                    });
+
                 }
-
             });
-        });
 
-    }));
+        }));
 
 };
