b94a3db717c1bc09599de18e8cd9ca8dc12accf4
Shan1024 <gambit1024@gmail.com>
2015-07-20 16:00:39 +0530

    Logic changed to display some basic info

--- test apps/Chrome-ext-Facebook-ID/inject.js
+++ test apps/Chrome-ext-Facebook-ID/inject.js
@@ -1,1 +1,15 @@
-document.getElementsByClassName("_2dpb")[0].innerHTML="dodamo"; 
+var myUsername = document.getElementsByClassName("_2dpe _1ayn")[0].href;
+
+console.log('My Username: ' + myUsername);
+
+var isPage = document.getElementsByClassName("_58gj fsxxl fwn fcw").length > 0;
+
+console.log("Is a page: "+isPage);
+
+if(!isPage){
+  var name = document.getElementById('fb-timeline-cover-name').innerText;
+  console.log("Name: "+name);
+}else{
+  var pageName = document.getElementsByClassName('_58gi _5rqs')[0].innerText;
+  console.log("Page Name: "+pageName);
+}
