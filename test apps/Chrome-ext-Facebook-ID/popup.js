131887fcf06b65a21fbd099a6ec9bb9a971d6e98
Shan1024 <gambit1024@gmail.com>
2015-07-20 15:59:57 +0530

    Removed unnecessary codes

--- test apps/Chrome-ext-Facebook-ID/popup.js
+++ test apps/Chrome-ext-Facebook-ID/popup.js
@@ -1,42 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
- 
+
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
- 
-      var f = d.createElement('form');
-      f.action = 'https://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-	  
+
+      // var f = d.createElement('form');
+      // f.action = 'https://gtmetrix.com/analyze.html?bm';
+      // f.method = 'post';
+
       var i = d.createElement('input');
       i.type = 'hidden';
       i.name = 'url';
       i.value = tab.url;
 	  if(i.value.includes("facebook")){
-		
+
 		/*chrome.tabs.duplicate(tab.id);*/
-		chrome.tabs.executeScript(tab.id,{
-			code:'document.body.style.backgroundColor="red";document.getElementsByClassName("_2dpb")[0].innerHTML;'
-		  },function(d){
-			alert(d);
-		  });
+
+		// chrome.tabs.executeScript(tab.id,{
+		// 	code:'document.body.style.backgroundColor="red";document.getElementsByClassName("_2dpb")[0].innerHTML;'
+		//   },function(d){
+		// 	     alert('Name: '+d);
+		//   });
+
 		  /*chrome.tabs.executeScript(tab.id,function(){
 			 document.body.style.backgroundColor="red"
 		  });*/
-		  
-		  
+
+
 		  chrome.tabs.executeScript(tab.id,{
 			/*code:'document.getElementsByClassName("_2dpb")[0].innerHTML="dodans";'*/
 			file:'inject.js'
 		  },function(){
-			
+        // alert('Successfull');
 		  });
-		  
+
 	  }else{
 		alert("Incorrect Page");
 	  }
-	  
+
 	  /*chrome.permissions.request({
           permissions: ['webRequest']
         }, function(granted) {
@@ -47,15 +49,15 @@ document.addEventListener('DOMContentLoaded', function() {
             alert("deined");
           }
         });*/
-	  
-	  
+
+
 	  /*
       f.appendChild(i);
       d.body.appendChild(f);
       f.submit();*/
     });
-	
-	
+
+
   }, false);
   /*chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
