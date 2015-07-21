2bf9f1eecff0daf26377d1131f0df8942aa4ae8f
pupudu <pubudu_pincode@live.com>
2015-07-21 19:26:30 +0530

    append new element to group, if a group

--- test apps/Chrome-ext-Facebook-ID/inject.js
+++ test apps/Chrome-ext-Facebook-ID/inject.js
@@ -3,13 +3,22 @@ var myUsername = document.getElementsByClassName("_2dpe _1ayn")[0].href;
 console.log('My Username: ' + myUsername);
 
 var isPage = document.getElementsByClassName("_58gj fsxxl fwn fcw").length > 0;
+var isGroup = (document.getElementsByClassName('_55pe')[0].innerText =="Joined");
 
 console.log("Is a page: "+isPage);
-
+if(!isGroup){
 if(!isPage){
   var name = document.getElementById('fb-timeline-cover-name').innerText;
   console.log("Name: "+name);
 }else{
   var pageName = document.getElementsByClassName('_58gi _5rqs')[0].innerText; //replaced '_58gi _5rqs' with '_58gi' , first one only for verified pages
   console.log("Page Name: "+pageName);
+}
+}
+
+if(isGroup){	//testing adding element to group name
+	document.getElementsByTagName('h2')[0].innerHTML = document.getElementsByTagName('h2')[0].innerHTML + '<a class="_5r2h" href="/groups/208051486009709/" dir="">Element Add Test</a>';
+	console.log('is a group');
+}else{
+	console.log('not a group');
 }
