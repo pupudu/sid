802ff7409adda4393cacb910a8a50e164c5af4b7
pupudu <pubudu_pincode@live.com>
2015-07-21 20:21:57 +0530

    append verified icon if a profile

--- test apps/Chrome-ext-Facebook-ID/inject.js
+++ test apps/Chrome-ext-Facebook-ID/inject.js
@@ -22,3 +22,10 @@ if(isGroup){	//testing adding element to group name
 }else{
 	console.log('not a group');
 }
+
+var isProfile = (document.getElementById('fb-timeline-cover-name').innerText.length > 0);
+if(isProfile){
+	document.getElementById('fb-timeline-cover-name').innerHTML = 
+		document.getElementById('fb-timeline-cover-name').innerHTML +
+		'<span class="_5rqt"><span class="_5rqu"><span data-hover="tooltip" data-tooltip-position="right" class="_56_f _5dzy _5d-1 _5d-3" id="u_jsonp_2_7" aria-label="Verified Page"></span></span></span>'
+}
