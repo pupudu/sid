6b1d6bcd4a5de0fea65e124e2d88f25f7fa06f66
pupudu <pubudu_pincode@live.com>
2015-07-21 21:28:14 +0530

    fixed bugs

--- test apps/Chrome-ext-Facebook-ID/inject.js
+++ test apps/Chrome-ext-Facebook-ID/inject.js
@@ -21,14 +21,13 @@ if(!isGroup){
 }
 
 if(isGroup){	//testing adding element to group name
-	document.getElementsByTagName('h2')[0].innerHTML = document.getElementsByTagName('h2')[0].innerHTML + '<a class="_5r2h" href="/groups/208051486009709/" dir="">Element Add Test</a>';
+	document.getElementsByTagName('h2')[0].innerHTML += '<a class="_5r2h" href="/groups/208051486009709/" dir="">Element Add Test</a>';
 	console.log('is a group');
 }else{
 	console.log('not a group');
 }
 
 if(isProfile){
-	document.getElementById('fb-timeline-cover-name').innerHTML = 
-		document.getElementById('fb-timeline-cover-name').innerHTML +
-		'<span class="_5rqt"><span class="_5rqu"><span data-hover="tooltip" data-tooltip-position="right" class="_56_f _5dzy _5d-1 _5d-3" id="u_jsonp_2_7" aria-label="Verified Page"></span></span></span>'
+	document.getElementById('fb-timeline-cover-name').innerHTML += 
+		'<span class="_5rqt"><span class="_5rqu"><span data-hover="tooltip" data-tooltip-position="right" class="_56_f _5dzy _5d-1 _5d-3" id="u_jsonp_2_7" aria-label="sID Verified User"></span></span></span>'
 }
